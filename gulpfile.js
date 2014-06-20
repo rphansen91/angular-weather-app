3ad85889bf149f5f21e5c2da28794d30af704dc5
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-06-20 16:49:12 +0100

    removed bower components from version control and deleted some comments

--- gulpfile.js
+++ gulpfile.js
@@ -51,29 +51,3 @@ gulp.task('connect', function() {
   });
 });
 gulp.task('default', ['connect']);
-
-/* 2 */
-// gulp.task('clean', function() {
-//     return gulp
-//       .src(['./index.html', 'assets/js/min'], { read:false })
-//       .pipe(clean());
-// });
-
-// gulp.task('html', ['clean'], function () {
-// var context = rev.Context();
-//   return gulp.src('./dev.html')
-//     .pipe(rename('index.html'))
-//     .pipe(usemin({ cssmin: false, htmlmin: false, jsmin: false }))
-//     .pipe(context.replace(/src="assets\/js\/min\/(\w+\.js)"/g, 'src="assets/js/min/{{$1}}"'))
-//     .pipe(gulp.dest('./'));
-// });
-
-// gulp.task('js', ['html'], function () {
-//   return gulp.src('assets/js/min/app.js') // FRAGILE: gulp-usemin creates this file
-//     .pipe(ngmin())
-//     .pipe(uglify())
-//     .pipe(rev())
-//     .pipe(gulp.dest('./assets/js/min/')),
-// });
-
-// gulp.task('default', ['js']);
